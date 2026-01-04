ceb0910bd71650651a92681b1491e259ac52bc62
Prajwal S Mallur <151111547+PrajwalSMallur@users.noreply.github.com>
2026-01-04 18:00:06 +0530

    commit message: Add test for hello method

--- src/test/java/com/example/employee/EmployeeApplicationTest.java
+++ src/test/java/com/example/employee/EmployeeApplicationTest.java
@@ -1,15 +1,12 @@
 package com.example.employee;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@SpringBootTest
 class EmployeeApplicationTest {
 
     @Test
-    void contextLoads() {
+    void testHello() {
         EmployeeApplication app = new EmployeeApplication();
         assertEquals("hello", app.hello());
     }
