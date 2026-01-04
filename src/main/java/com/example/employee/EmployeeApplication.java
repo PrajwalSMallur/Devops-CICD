cd6ac6458c3267a8760aba1fd2e6749e27121c2c
Prajwal S Mallur <151111547+PrajwalSMallur@users.noreply.github.com>
2026-01-04 17:49:32 +0530

    Fix missing Spring Boot imports

--- src/main/java/com/example/employee/EmployeeApplication.java
+++ src/main/java/com/example/employee/EmployeeApplication.java
@@ -1,3 +1,8 @@
+package com.example.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class EmployeeApplication {
 
@@ -5,7 +10,7 @@ public class EmployeeApplication {
         SpringApplication.run(EmployeeApplication.class, args);
     }
 
-    // NEW CODE (this is important)
+    // New code (for Sonar coverage)
     public String hello() {
         return "hello";
     }
