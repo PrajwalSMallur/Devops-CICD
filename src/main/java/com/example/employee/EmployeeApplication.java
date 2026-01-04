a5e08210ab39d12b7c4ec6746b59d84e03877ee1
Prajwal S Mallur <151111547+PrajwalSMallur@users.noreply.github.com>
2026-01-04 14:43:50 +0530

    Update EmployeeApplication.java

--- src/main/java/com/example/employee/EmployeeApplication.java
+++ src/main/java/com/example/employee/EmployeeApplication.java
@@ -1,11 +1,13 @@
 package com.example.employee;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class EmployeeApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EmployeeApplication.class, args);
+@SpringBootTest
+class EmployeeApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // application context loads successfully
     }
 }
